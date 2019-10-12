ffb57730424e8b32a79454b8a792bdb4100edc62
Simone Magodi <asche.mago@gmail.com>
2019-10-12 13:30:38 +0200

    Add checkbox for cells editability.

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ var $TABLE = $('#table');
 var $BTN = $('#export-btn');
 var $BTN2 = $('#import-btn');
 var $EXPORT = $('#export');
+var $CHECK = $('#myCheck');
+var $TEXT = $('#text');
 
-$('.table-add').click(function () {
+$('.table-add').click(function(){
   var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
   $TABLE.find('table').append($clone);
+  f_checkbox();
 });
 
 $('.table-remove').click(function () {
@@ -27,7 +30,7 @@ $('.table-down').click(function () {
 jQuery.fn.pop = [].pop;
 jQuery.fn.shift = [].shift;
 
-function validate_input(value, column){
+function f_validateinput(value, column){
   var impegnoRGEX = /^[0-9]{0,1}[0-9]{0,1}[0-9]{0,1}[0-9]{0,1}[-\s\/]{1}[0-9]{2}$/;
   var articoloRGEX = /^[0-9]{1}[A-Z]{3}[0-9]{5}/;
 
@@ -68,7 +71,7 @@ $BTN.click(function () {
     // Use the headers from earlier to name our hash keys
     headers.forEach(function (header, i) {
       h[header] = $td.eq(i).text();
-      if(!validate_input(h[header],i)){
+      if(!f_validateinput(h[header],i)){
         alert("Formato sbagliato " + header + " riga " + x.rowIndex);
       }
     });
@@ -120,44 +123,7 @@ $BTN2.click(function(){
   data.push(h);
   });
 
-  $EXPORT.text($rows.length);
-  /*
-  // Create one new row for every array row
-  for (var i = 0; i < test_arr.length; i++){
-    var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
-    $TABLE.find('table').append($clone);
-    var obj = test_arr[i];
-    for (var key in obj){
-      var value = obj[key];
-      //headers.push(key);
-      data.push(value);
-      //$TABLE.rows[i+1].cells[1].innerHTML = value;
-    }
-  }
-
-  var $rows = $TABLE.find('tr:not(:hidden)');
-
-  // Get the headers (add special header logic here)
-  $($rows.shift()).find('th:not(:empty)').each(function () {
-    headers.push($(this).text().toLowerCase());
-  });
-
-  // Turn all existing rows into a loopable array
-
-  $rows.each(function () {
-    var $td = $(this).find('td');
-    var h = {};
-
-      // Use the headers from earlier to name our hash keys
-      //data.forEach(function (element,x) {
-        //$td.text()="1";
-      //headers.push(element);
-      //});
-
-      //data.push(h);
-  });*/
-
-
+  //$EXPORT.text($rows.length);
 
   $EXPORT.text(JSON.stringify(test_arr));
 });
@@ -165,9 +131,19 @@ $BTN2.click(function(){
 function f_checkbox(){
   var checkBox = document.getElementById("myCheck");
   var text = document.getElementById("text");
+  $rows = $TABLE.find('tr:not(:hidden)');
   if (checkBox.checked == true){
-    text.style.display = "block";
+    //text.style.display = "block";
+    //text.contentEditable = "true";
+    $rows.each(function(){
+      $(this).find('td').attr('contenteditable','true');
+    });
+
   } else {
-     text.style.display = "none";
+     //text.style.display = "none";
+     //text.contentEditable = "false";
+     $rows.each(function(){
+       $(this).find('td').attr('contenteditable','false');
+     });
   }
 }
