9b3b82f0710350b2cd72ac5b02a21bd2d446409f
Tecnico6 <tecnico6@DOMINIO.DOGICAN>
2019-10-19 10:51:49 +0200

    Improved code readability

---
+++ script2.js
@@ -0,0 +1,10 @@
+$("p").css('border', '1px solid red');
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
