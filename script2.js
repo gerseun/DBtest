9ee1cf07397f3e12113c728a3a3f7e6886f85068
Tecnico6 <tecnico6@DOMINIO.DOGICAN>
2019-10-21 17:27:37 +0200

    Improved code readability.

--- script2.js
+++ script2.js
@@ -14,23 +14,142 @@ $(document).ready(function(){
 function f_checkbox(){
   if ($("#myCheck").prop('checked')){
     $('[contenteditable]').attr('contenteditable','true');
+    $('#update-btn').removeClass('hide')
   } else {
     $('[contenteditable]').attr('contenteditable','false');
+    $('#update-btn').addClass('hide','true');
   }
 }
 
 // Add new row on click
-$("#table-add").click(function(){
-  alert("clicked");
-  //var $clone = $('#table').find('tr.hide').clone(true).removeClass('hide table-line');
-  //$('#table').find('table').append($clone);
+$('.table-add').click(function(){
+  var $clone = $('#table').find('tr.hide').clone(true).removeClass('hide table-line');
+  $('#table').find('table').append($clone);
   //f_checkbox();
+});
+
+// Remove row on click
+$('.table-remove').click(function () {
+  $(this).parents('tr').detach();
 });
 
 // A few jQuery helpers for exporting only
 jQuery.fn.pop = [].pop;
 jQuery.fn.shift = [].shift;
+jQuery.fn.reverse = [].reverse;
 
-$("#export-btn").click(function(event) {
-  alert("suka");
+// Export table data
+$('#export-btn').click(function(event) {
+  var $rows = $('#table').find('tr:not(:hidden)');
+  var headers = [];
+  var data = [];
+  // Get the headers (add special header logic here)
+  $('.header').find('th:not(.control)').each(function (index) {
+    headers.push($(this).text()/*.toLowerCase()*/);
+  });
+  $rows.shift();
+  // Turn all existing rows into a loopable array
+  $rows.each(function (row,x) {
+    var $td = $(this).find('td');
+    var h = {};
+
+    // Use the headers from earlier to name our hash keys
+    headers.forEach(function (header, i) {
+      h[header] = $td.eq(i).text();
+      // Check input format
+      if(!f_validateinput(h[header],i)){
+        alert("Formato sbagliato " + header + " riga " + x.rowIndex);
+      }
+    });
+
+  data.push(h);
+  });
+
+  // Output the result
+  $('#output').text(JSON.stringify(data));
+  //$('#output').text('export');
 });
+
+// Import from json
+test_arr = [
+  {"Impegno":"1","Articolo":"2","Q.tà Articolo":"3","Componente":"4","Q.tà Componente":"5","Stato":"6"},
+  {"Impegno":"a","Articolo":"b","Q.tà Articolo":"c","Componente":"d","Q.tà Componente":"e","Stato":"f"}
+];
+
+// Import table
+$('#import-btn').click(function(event) {
+  var $rows = $('#table').find('tr:not(:hidden)');
+  var headers = [];
+  var data = [];
+
+  // Check JSON length and add rows
+  if($rows.length <= test_arr.length){
+  for (var i = 1; i < test_arr.length; i++){
+    var $clone = $('#table').find('tr.hide').clone(true).removeClass('hide table-line');
+    $('#table').find('table').append($clone);
+  }}else{
+    $rows.each(function(index){
+      if(index > test_arr.length){
+        $(this).detach();
+      }
+    });
+  }
+  $rows = $('#table').find('tr:not(:hidden)');
+  // Get the headers (add special header logic here)
+  $('.header').find('th:not(.control)').each(function (index) {
+    headers.push($(this).text()/*.toLowerCase()*/);
+  });
+  $rows.shift();
+  // Turn all existing rows into a loopable array
+  $rows.each(function (index) {
+    var $td = $(this).find('td');
+      var h = {};
+    var obj = test_arr[index];
+
+    // Use the headers from earlier to name our hash keys
+    headers.forEach(function (header, i) {
+      $td.eq(i).text(obj[header]);
+    });
+
+  data.push(h);
+  });
+
+  //$EXPORT.text($rows.length);
+
+  $('#output').text(JSON.stringify(test_arr));
+});
+
+
+$('#update-btn').click(function(event) {
+  $('#output').text('update');
+});
+
+// Check input format
+function f_validateinput(value, column){
+  /*
+  var impegnoRGEX = /^[0-9]{0,1}[0-9]{0,1}[0-9]{0,1}[0-9]{0,1}[-\s\/]{1}[0-9]{2}$/;
+  var articoloRGEX = /^[0-9]{1}[A-Z]{3}[0-9]{5}/;
+  var quantitaRGEX = /^[0-9]{1}[0-9]{0,1}[0-9]{0,1}[0-9]{0,1}$/;
+  // Validate Impegno format. Accepted format (1111-11) (1111 11) (1111/11)
+  if(column==0){
+    var checkResult = impegnoRGEX.test(value);
+    if(!checkResult){
+      return false;
+    }
+  }
+  // Validate Articolo format. Accepted format (1AAA11111...)
+  if(column==1){
+    var checkResult = articoloRGEX.test(value);
+    if(!checkResult){
+      return false;
+    }
+  }
+  // Validate Quantità format. Accepted format (1111)
+  if (column==2 || column==4) {
+    var checkResult = quantitaRGEX.test(value);
+    if(!checkResult){
+      return false;
+    }
+  }*/
+  return true;
+}
